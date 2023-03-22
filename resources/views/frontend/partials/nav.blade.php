1e56d8fa416d2112ab84cf0e5ac2e990d9e44d7b
NafisaNitu <nafisanitu240@gmail.com>
2023-03-22 15:51:10 +0600

    ad store website

---
+++ resources/views/frontend/partials/nav.blade.php
@@ -0,0 +1,97 @@
+<header class="sticky-top z-1020 bg-white shadow-sm">
+    <div
+     class="position-relative logo-bar-area z-1 bg-white">
+        <!--bg-white-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                     <div class="col-auto col-xl-3 pl-0 pr-3 d-flex align-items-center">
+                         <a class="d-block py-2 mr-3 ml-0" href="https://sobkisubazar.com">
+                             <img src="{{ asset('frontend/assets/images/logo.png') }}" 
+                                alt="Sobkisubazar" class="mw-120 h-51px h-md-51px">
+                         </a>
+                     </div>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <div class="flex-grow-1 mr-5 align-items-center d-lg-block">
+                         <div class="position-relative flex-grow-1">
+                            <!-- <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                              </form> -->
+                             <form action="" method="GET" class="mb-0 mobile-hor-swipe text-center">
+                                 <div class="d-lg-block d-flex position-relative align-items-center">
+                                     <div class="input-group">
+                                         <input type="text" class="form-control" style="font-size: 18px;"
+                                              placeholder="Search Your Product">
+                                         <div class="d-lg-block">
+                                             <button class="btn btn-primary" type="submit"
+                                                 style="background-color:#572c84!important;border-color:#572c84!important">
+                                                 <i style="line-height:20px !important;"
+                                                     class="fa-solid fa-magnifying-glass fs-18"></i>
+                                             </button>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </form>
+                         </div>
+                     </div>
+ 
+                     <div class="d-lg-block ml-3 mr-0">
+                        <ul class="list-inline mb-0 mobile-hor-swipe text-center navbar-nav">
+                            <li class="list-inline-item mr-0 py-1 nav-item">
+                                <a
+                                    href="#"
+                                    class="ad-menu px-2 py-2 d-inline-block text-reset">
+                                    <span class="menu-text-hover">All Ads</span>
+                                </a>
+                            </li>
+                        </ul>
+                     </div>
+ 
+                     <div class="d-lg-block ml-3 mr-0">
+                        <ul class="list-inline mb-0 mobile-hor-swipe text-center">
+                            <li class="list-inline-item mr-0 py-1">
+                                <a
+                                    href="#"
+                                    class="ad-menu px-2 py-2 d-inline-block text-reset">
+                                    <span class="menu-text-hover">Support</span>
+                                </a>
+                            </li>
+                        </ul>
+                     </div>
+ 
+                     <div class="d-lg-block ml-3 mr-0">
+                        <ul class="list-inline mb-0 mobile-hor-swipe text-center">
+                            <li class="list-inline-item mr-0 py-1">
+                                <a
+                                    href="#"
+                                    class="ad-menu px-2 py-2 d-inline-block text-reset">
+                                    <span class="menu-text-hover">My Account</span>
+                                </a>
+                            </li>
+                        </ul>
+                     </div>
+                     <div class="d-lg-block ml-3 mr-0">
+                         <ul class="list-inline mb-0 mobile-hor-swipe text-center">
+                             <li class="list-inline-item mr-0">
+                                 <a href="{{ route('adstore') }}" target="_blank">
+                                     <button type="button" class="btn btn-post btn-rounded">Post Your Ad</button>
+                                 </a>
+                             </li>
+                            </ul>
+                     </div>
+                     </div>
+                    </nav>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/frontend/partials/script.blade.php
@@ -0,0 +1,261 @@
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+<script src ="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script>
+
+<script src ="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"> </script>
+<script src="{{ asset('frontend/assets/js/vendors.js') }}"></script>
+<script src="{{ asset('frontend/assets/js/bootstrap.min.js') }}"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/fontawesome.min.js"
+    integrity="sha512-j3gF1rYV2kvAKJ0Jo5CdgLgSYS7QYmBVVUjduXdoeBkc4NFV4aSRTi+Rodkiy9ht7ZYEwF+s09S43Z1Y+ujUkA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.js"></script>
+<!-- <script src="js/main.js"></script> -->
+
+<script>
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            if (this.scrollY > 10) {
+                $('.sidebar').addClass("sticky");
+            } else {
+                $('.sidebar').removeClass("sticky");
+            }
+        });
+    });
+</script>
+
+<!-- For Sale Section -->
+<script>
+    $(document).ready (function () {
+      $(".content").slice(0, 4).show();
+      $("#loadMore").on("click", function(e){
+        e.preventDefault();
+        $(".content:hidden").slice(0, 4).slideDown();
+        if ($(".content:hidden").length == 0) {
+          $("#loadMore").text("No Content").addClass("noContent");
+        }
+      });
+      })
+</script>
+<!-- For Buy Section -->
+ <script>
+    $(document).ready (function () {
+      $(".contentBuy").slice(0, 4).show();
+      $("#loadMoreBuy").on("click", function(e){
+        e.preventDefault();
+        $(".contentBuy:hidden").slice(0, 4).slideDown();
+        if ($(".contentBuy:hidden").length == 0) {
+          $("#loadMoreBuy").text("No Content").addClass("noContent");
+        }
+      });
+      })
+</script>
+
+ <!-- For Rent Section -->
+ <script>
+    $(document).ready (function () {
+      $(".contentRent").slice(0, 4).show();
+      $("#loadMoreRent").on("click", function(e){
+        e.preventDefault();
+        $(".contentRent:hidden").slice(0, 4).slideDown();
+        if ($(".contentRent:hidden").length == 0) {
+          $("#loadMoreRent").text("No Content").addClass("noContent");
+        }
+      });
+      })
+</script>
+
+<script>
+    $('#search').on('keyup', function() {
+        search();
+    });
+
+    $('#search').on('focus', function() {
+        search();
+    });
+
+    function search() {
+        var searchKey = $('#search').val();
+        if (searchKey.length > 0) {
+            $('body').addClass("typed-search-box-shown");
+
+            $('.typed-search-box').removeClass('d-none');
+            $('.search-preloader').removeClass('d-none');
+            $.post('https://sobkisubazar.com/ajax-search', {
+                _token: AIZ.data.csrf,
+                search: searchKey
+            }, function(data) {
+                if (data == '0') {
+                    // $('.typed-search-box').addClass('d-none');
+                    $('#search-content').html(null);
+                    $('.typed-search-box .search-nothing').removeClass('d-none').html(
+                        'Sorry, nothing found for <strong>"' + searchKey + '"</strong>');
+                    $('.search-preloader').addClass('d-none');
+
+                } else {
+                    $('.typed-search-box .search-nothing').addClass('d-none').html(null);
+                    $('#search-content').html(data);
+                    $('.search-preloader').addClass('d-none');
+                }
+            });
+        } else {
+            $('.typed-search-box').addClass('d-none');
+            $('body').removeClass("typed-search-box-shown");
+        }
+    }
+
+</script>
+
+<script>
+    //Get the button
+    var mybutton = document.getElementById("myBtn");
+
+    /* When the user scrolls down 20px from the top of the document, show the button */
+    window.onscroll = function() {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        // document.body.scrollTop = 0 ;
+        // document.documentElement.scrollTop = 0;
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+</script>
+
+<script>
+    $(document).ready(function(){
+
+if($('.bbb_viewed_slider').length)
+{
+    var viewedSlider = $('.bbb_viewed_slider');
+
+    viewedSlider.owlCarousel(
+        {
+            loop:true,
+            margin:30,
+            autoplay:true,
+            autoplayTimeout:1500,
+            nav:false,
+            dots:false,
+            responsive:
+                {
+                    0:{items:1},
+                    575:{items:3},
+                    767:{items:3},
+                    768:{items:4},
+                    992:{items:4},
+                    1370:{items:5}
+                }
+        });
+
+    if($('.bbb_viewed_prev').length)
+    {
+        var prev = $('.bbb_viewed_prev');
+        prev.on('click', function()
+        {
+            viewedSlider.trigger('prev.owl.carousel');
+        });
+    }
+
+    if($('.bbb_viewed_next').length)
+    {
+        var next = $('.bbb_viewed_next');
+        next.on('click', function()
+        {
+            viewedSlider.trigger('next.owl.carousel');
+        });
+    }
+}
+
+
+});
+</script>
+
+<script>
+$(document).ready(function(){
+
+if($('.bbb_viewed_slider_2').length)
+{
+var viewedSlider = $('.bbb_viewed_slider_2');
+
+viewedSlider.owlCarousel(
+    {
+        loop:true,
+        margin:30,
+        autoplay:true,
+        autoplayTimeout:2000,
+        nav:false,
+        dots:false,
+        responsive:
+            {
+
+                    0:{items:1},
+                    575:{items:3},
+                    767:{items:3},
+                    768:{items:4},
+                    992:{items:4},
+                    1370:{items:5}
+
+                    // 0:{items:1},
+                    // 575:{items:2},
+                    // 768:{items:3},
+                    // 991:{items:3},
+                    // 1199:{items:5}
+            }
+    });
+
+if($('.bbb_viewed_prev').length)
+{
+    var prev = $('.bbb_viewed_prev');
+    prev.on('click', function()
+    {
+        viewedSlider.trigger('prev.owl.carousel');
+    });
+}
+
+if($('.bbb_viewed_next').length)
+{
+    var next = $('.bbb_viewed_next');
+    next.on('click', function()
+    {
+        viewedSlider.trigger('next.owl.carousel');
+    });
+}
+}
+
+
+});
+</script>
+
+
+<script>
+function change() {
+let results = Array.from(document.querySelectorAll('.result > div'));
+// Hide all results
+results.forEach(function(result) {
+    result.style.display = 'none';
+});
+// Filter results to only those that meet ALL requirements:
+Array.from(document.querySelectorAll('.filter input[rel]:checked'), function(input) {
+    const attrib = input.getAttribute('rel');
+    results = results.filter(function(result) {
+    return result.classList.contains(attrib);
+    });
+});
+// Show those filtered results:
+results.forEach(function(result) {
+    result.style.display = 'block';
+});
+}
+change();
+</script>
