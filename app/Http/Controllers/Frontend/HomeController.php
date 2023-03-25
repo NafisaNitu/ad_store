6906251f90bf88eb70d0750c965059d6ed2ee40b
haidercse <haider.cse7644@gmail.com>
2023-03-25 09:58:03 +0600

    add store

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -13,30 +13,45 @@ class HomeController extends Controller
         $response = Http::get('https://sobkisubazar.com/api/v2/sliders');
         $platinum = Http::get('https://sobkisubazar.com/api/v2/platinum');
         $advertisement = Http::get('https://sobkisubazar.com/api/v2/advertisement');
+        $ad_store = Http::get('https://sobkisubazar.com/api/v2/ad-store');
 
         $data = json_decode($response, true);
         $platinum = json_decode($platinum, true);
         $advertisement = json_decode($advertisement, true);
+        $ad_stores = json_decode($ad_store, true);
 
 
         $platinum_data = [];
         $advertisement_data = [];
+        $ad_stores_data = [];
+
+        //sobkicubazar slider
         if ($data['status'] == '200') {
             $data = $data['data'];
         } else {
             $data = [];
         }
+
+        //platinum package
         foreach ($platinum['data'] as $pl) {
             $platinum_data[] = [
                 'photo' => 'https://sobkisubazar.com/public/' . $pl['photo'],
             ];
         }
+
+        //advertiesment package
         foreach ($advertisement['data'] as $ad) {
             $advertisement_data[] = [
                 'photo' => 'https://sobkisubazar.com/public/' . $ad['photo'],
             ];
         }
-        return view('frontend.pages.index', compact('data', 'platinum_data', 'advertisement_data'));
+        //adstore package
+        foreach ($ad_stores['data'] as $ad_store) {
+            $ad_stores_data[] = [
+                'photo' => 'https://sobkisubazar.com/public/' . $ad_store['photo'],
+            ];
+        }
+        return view('frontend.pages.index', compact('data', 'platinum_data', 'advertisement_data','ad_stores_data'));
     }
     public function adstore()
     {
